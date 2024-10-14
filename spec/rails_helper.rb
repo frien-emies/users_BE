3fcb9c71f8ac1362cc9cbe1cb300b410daf904ff
Lance Nelson <lancen04@gmail.com>
2024-10-14 13:39:05 -0400

    add: added code chunks to enable functionality for testing tools. Also created directories in /spec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,9 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'simplecov'
 require 'spec_helper'
+
+SimpleCov.start
+
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
@@ -65,4 +69,11 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
 end
