1325c3641525a0e2f2aa7d8e256ab08f1beab103
Cory Bretsch <corybretsch@gmail.com>
2024-10-17 17:55:56 -0500

    added edge case testing for users_search

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::UsersController < ApplicationController
 
   def index
     users = User.search(params[:query])
-    render json: UserSerializer.new(users), status: 200
+    render json: UserSerializer.new(users)
   end
 end
--- spec/requests/api/v1/users_search_spec.rb
+++ spec/requests/api/v1/users_search_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Users Search - Index', type: :request do
-  describe 'returns user data' do
+  describe 'returns user data - happy paths' do
     it 'can return users by partial search on username' do
       user1 = User.create(username: "bob", email: "bob@turing.com", password: "ihateruby", avatar: "image1_string_goes_here.jpg")
       user2 = User.create(username: "rob", email: "rob@turing.com", password: "iloveruby", avatar: "image2_string_goes_here.jpg")
@@ -29,7 +29,7 @@ RSpec.describe 'Users Search - Index', type: :request do
       expect(first[:attributes][:avatar]).to eq("image4_string_goes_here.jpg")
     end
 
-    it 'can return users by partial search on username' do
+    it 'can return all users when search criteria is blank' do
       user1 = User.create(username: "bob", email: "bob@turing.com", password: "ihateruby", avatar: "image1_string_goes_here.jpg")
       user2 = User.create(username: "rob", email: "rob@turing.com", password: "iloveruby", avatar: "image2_string_goes_here.jpg")
       user3 = User.create(username: "knob", email: "knob@turing.com", password: "iworshipruby", avatar: "image3_string_goes_here.jpg")
@@ -43,49 +43,48 @@ RSpec.describe 'Users Search - Index', type: :request do
       expect(response.status).to eq 200
 
       users = JSON.parse(response.body, symbolize_names: true)[:data]
-require 'pry'; binding.pry
-      expect(users.count).to eq(2)
 
-      first = users.first
-
-      expect(first[:type]).to eq("user")
-      expect(first[:id]).to eq(user4.id.to_s)
-      expect(first[:attributes][:username]).to eq("jan")
-      expect(first[:attributes][:email]).to eq("jan@turing.com")
-      expect(first[:attributes][:avatar]).to eq("image4_string_goes_here.jpg")
+      expect(users.count).to eq(7)
     end
+
+    it 'can return no users' do
+      user1 = User.create(username: "bob", email: "bob@turing.com", password: "ihateruby", avatar: "image1_string_goes_here.jpg")
+      user2 = User.create(username: "rob", email: "rob@turing.com", password: "iloveruby", avatar: "image2_string_goes_here.jpg")
+      user3 = User.create(username: "knob", email: "knob@turing.com", password: "iworshipruby", avatar: "image3_string_goes_here.jpg")
+      user4 = User.create(username: "jan", email: "jan@turing.com", password: "imabeast", avatar: "image4_string_goes_here.jpg")
+      user5 = User.create(username: "kat", email: "kat@turing.com", password: "bestinstructorever", avatar: "image5_string_goes_here.jpg")
+      user6 = User.create(username: "abdul", email: "abdul@turing.com", password: "kingofmod2", avatar: "image6_string_goes_here.jpg")
+      user7 = User.create(username: "jeff", email: "jeff@turing.com", password: "iownthisshizz", avatar: "image7_string_goes_here.jpg")
+      
+      query = "1"
+      get "/api/v1/users/search?query=#{query}"
+
+      expect(response.status).to eq(200)
+
+      users = JSON.parse(response.body, symbolize_names: true)[:data]
+      require 'pry'; binding.pry
+      expect(users.count).to eq(0)
+    end
+
+    # describe "SAD PATH" do
+    #   it 'can return error message for bad URI' do
+    #     user1 = User.create(username: "bob", email: "bob@turing.com", password: "ihateruby", avatar: "image1_string_goes_here.jpg")
+    #     user2 = User.create(username: "rob", email: "rob@turing.com", password: "iloveruby", avatar: "image2_string_goes_here.jpg")
+    #     user3 = User.create(username: "knob", email: "knob@turing.com", password: "iworshipruby", avatar: "image3_string_goes_here.jpg")
+    #     user4 = User.create(username: "jan", email: "jan@turing.com", password: "imabeast", avatar: "image4_string_goes_here.jpg")
+    #     user5 = User.create(username: "kat", email: "kat@turing.com", password: "bestinstructorever", avatar: "image5_string_goes_here.jpg")
+    #     user6 = User.create(username: "abdul", email: "abdul@turing.com", password: "kingofmod2", avatar: "image6_string_goes_here.jpg")
+    #     user7 = User.create(username: "jeff", email: "jeff@turing.com", password: "iownthisshizz", avatar: "image7_string_goes_here.jpg")
+        
+    #     query = "{}"
+    #     get "/api/v1/users/search?query=#{query}"
+    #     require 'pry'; binding.pry
+    #     expect(response.status).to eq 204
+  
+    #     users = JSON.parse(response.body, symbolize_names: true)[:data]
+  
+    #     expect(users.count).to eq(0)
+    #   end
+    # end
   end
 end
-
-
-
-  
-  #   get "/api/v0/gratitudes/#{@gratitude1.user_id}/dates"
-
-  #   dates = JSON.parse(response.body, symbolize_names: true)[:data]
-
-  #   
-
-  #   dates.each do |date|
-  #     expect(date).to have_key(:attributes)
-  #     expect(date[:attributes]).to be_a(Hash)
-
-  #     expect(date[:attributes]).to have_key(:user_id)
-  #     expect(date[:attributes][:user_id]).to be_a(String)
-
-  #     expect(date[:attributes]).to have_key(:date)
-  #     expect(date[:attributes][:date]).to be_a(String)
-
-  #     expect(date[:attributes][:quote]).to have_key(:affirmation)
-  #     expect(date[:attributes][:quote]).to be_a(Hash)
-
-  #     expect(date[:attributes]).to have_key(:entry)
-  #     expect(date[:attributes][:entry]).to be_a(String)
-  #   end
-
-  #   first_date = dates.first
-
-  #   expect(first_date[:attributes][:user_id]).to eq("1")
-  #   expect(first_date[:attributes][:date]).to eq("2024-09-11")
-  #   expect(first_date[:attributes][:quote][:affirmation]).to be_a(String)
-  # end
