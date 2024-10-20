0fb9529b87dc300361cc41a1b68117aa4cf20179
Cory Bretsch <corybretsch@gmail.com>
2024-10-20 18:55:34 -0500

    adding circle_ci config.yml file and allowing all origins in CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://users-fe-app.vercel.app/"
+    origins '*'
+    # origins "https://users-fe-app.vercel.app/"
+    # "/var/run/postgresql/.s.PGSQL.5432"
 
     resource "*",
       headers: :any,
