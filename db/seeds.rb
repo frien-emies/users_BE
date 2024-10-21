b00985bb0f982bc41f4a128b1bc8ef9ed03e620a
Lance Nelson <lancen04@gmail.com>
2024-10-21 17:46:09 -0400

    added images to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,13 +13,13 @@ Friendship.destroy_all
 User.destroy_all
 Game.destroy_all
 
-user1 = User.create(username: "bob", email: "bob@turing.com", password: "ihateruby", avatar: "image1_string_goes_here.jpg")
-user2 = User.create(username: "rob", email: "rob@turing.com", password: "iloveruby", avatar: "image2_string_goes_here.jpg")
-user3 = User.create(username: "knob", email: "knob@turing.com", password: "iworshipruby", avatar: "image3_string_goes_here.jpg")
-user4 = User.create(username: "jan", email: "jan@turing.com", password: "imabeast", avatar: "image4_string_goes_here.jpg")
-user5 = User.create(username: "kat", email: "kat@turing.com", password: "bestinstructorever", avatar: "image5_string_goes_here.jpg")
-user6 = User.create(username: "abdul", email: "abdul@turing.com", password: "kingofmod2", avatar: "image6_string_goes_here.jpg")
-user7 = User.create(username: "jeff", email: "jeff@turing.com", password: "iownthisshizz", avatar: "image7_string_goes_here.jpg")
+user1 = User.create(username: "bob", email: "bob@turing.com", password: "ihateruby", avatar: "app/assets/images/baby.jpg")
+user2 = User.create(username: "rob", email: "rob@turing.com", password: "iloveruby", avatar: "app/assets/images/queen.jpg")
+user3 = User.create(username: "knob", email: "knob@turing.com", password: "iworshipruby", avatar: "app/assets/images/elephant.jpg")
+user4 = User.create(username: "jan", email: "jan@turing.com", password: "imabeast", avatar: "app/assets/images/dog.jpg")
+user5 = User.create(username: "kat", email: "kat@turing.com", password: "bestinstructorever", avatar: "app/assets/images/cat.jpg")
+user6 = User.create(username: "abdul", email: "abdul@turing.com", password: "kingofmod2", avatar: "app/assets/images/queen.jpg")
+user7 = User.create(username: "jeff", email: "jeff@turing.com", password: "iownthisshizz", avatar: "app/assets/images/bones.jpg")
 
 friendship1 = Friendship.create(user_id: user1.id, friend_id: user2.id)
 friendship2 = Friendship.create(user_id: user1.id, friend_id: user3.id)
