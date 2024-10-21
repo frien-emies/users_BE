8afa30465815aa1bc77cf3b0fe4e925885d6a9da
Cory Bretsch <corybretsch@gmail.com>
2024-10-20 19:26:02 -0500

    added environment var to config.yml

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
     # origins "https://users-fe-app.vercel.app/"
-    # "/var/run/postgresql/.s.PGSQL.5432"
+    # "/var/run/postgresql/.s.PGSQL.5432" OR "tcp://localhost:5432"
 
     resource "*",
       headers: :any,
