c29c1707c2433d68d1b1c742fddffe6a56fa5b93
Lance Nelson <lancen04@gmail.com>
2024-10-21 17:27:00 -0400

    add: added sad path testing for login

--- spec/requests/api/v1/user_login_spec.rb
+++ spec/requests/api/v1/user_login_spec.rb
@@ -30,4 +30,61 @@ RSpec.describe 'User Login Request', type: :request do
     expect(json[:attributes][:avatar]).to be_a(String)
     expect(json[:attributes][:avatar]).to eq(user.avatar)
   end
+
+### - SAD PATH >:C - ###
+  it "renders and error json if there is no email in the database" do
+    user = User.create(username: "bob", email: "bob@turing.com", password: "ihateruby", avatar: "image1_string_goes_here.jpg")
+
+    post "/api/v1/sessions", params: { email: "billybob@farmersonly.com", password: user.password } 
+
+    expect(response.status).to eq(400)
+
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(json).to have_key(:errors)
+    expect(json[:errors]).to be_a(String)
+    expect(json[:errors]).to eq("Username or password is incorrect.")
+  end
+
+  it "renders an error json if the passwords do not match" do
+    user = User.create(username: "bob", email: "bob@turing.com", password: "ihateruby", avatar: "image1_string_goes_here.jpg")
+
+    post "/api/v1/sessions", params: { email: user.email, password: "ihateruby321" } 
+
+    expect(response.status).to eq(400)
+
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(json).to have_key(:errors)
+    expect(json[:errors]).to be_a(String)
+    expect(json[:errors]).to eq("Username or password is incorrect.")
+  end
+
+  it "renders an error json if no email is inputted" do
+    user = User.create(username: "bob", email: "bob@turing.com", password: "ihateruby", avatar: "image1_string_goes_here.jpg")
+
+    post "/api/v1/sessions", params: { email: "", password: "iloveruby123" } 
+
+    expect(response.status).to eq(400)
+
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(json).to have_key(:errors)
+    expect(json[:errors]).to be_a(String)
+    expect(json[:errors]).to eq("Username or password is incorrect.")
+  end
+
+  it "renders an error json if no password is inputted" do
+    user = User.create(username: "bob", email: "bob@turing.com", password: "ihateruby", avatar: "image1_string_goes_here.jpg")
+
+    post "/api/v1/sessions", params: { email: user.email, password: "" } 
+
+    expect(response.status).to eq(400)
+
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(json).to have_key(:errors)
+    expect(json[:errors]).to be_a(String)
+    expect(json[:errors]).to eq("Username or password is incorrect.")
+  end
 end
