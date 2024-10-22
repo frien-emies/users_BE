7a560701701fedaa2743043c30a3a3a99324d9ea
Lance Nelson <lancen04@gmail.com>
2024-10-22 17:18:24 -0400

    edit: changed CORS origins to all

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://users-fe-app.vercel.app/"
+    origins "*"
 
     resource "*",
       headers: :any,
