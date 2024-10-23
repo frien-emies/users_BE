11545cac68554639da45feae6b6e0351c0c9058e
Lance Nelson <lancen04@gmail.com>
2024-10-23 16:23:02 -0400

    add: fixed seeds with image paths

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,13 @@ Friendship.destroy_all
 User.destroy_all
 Game.destroy_all
 
-user1 = User.create(username: "bob", email: "bob@turing.com", password: "ihateruby", avatar: "https://img.freepik.com/premium-photo/cartoon-skeleton-playing-chess-with-hat-it_406811-60002.jpg?semt=ais_hybrid")
+user1 = User.create(username: "bob", email: "bob@turing.com", password: "ihateruby", avatar: "https://chess-with-frein-emies-e45d9fb62d80.herokuapp.com/images/baby.jpg")
 user2 = User.create(username: "rob", email: "rob@turing.com", password: "iloveruby", avatar: "https://chess-with-frein-emies-e45d9fb62d80.herokuapp.com/images/queen.jpg")
-user3 = User.create(username: "knob", email: "knob@turing.com", password: "iworshipruby", avatar: "public/images/elephant.jpg")
-user4 = User.create(username: "jan", email: "jan@turing.com", password: "imabeast", avatar: "public/images/dog.jpg")
-user5 = User.create(username: "kat", email: "kat@turing.com", password: "bestinstructorever", avatar: "public/images/cat.jpg")
-user6 = User.create(username: "abdul", email: "abdul@turing.com", password: "kingofmod2", avatar: "public/images/queen.jpg")
-user7 = User.create(username: "jeff", email: "jeff@turing.com", password: "iownthisshizz", avatar: "public/images/bones.jpg")
+user3 = User.create(username: "knob", email: "knob@turing.com", password: "iworshipruby", avatar: "https://chess-with-frein-emies-e45d9fb62d80.herokuapp.com/images/elephant.jpg")
+user4 = User.create(username: "jan", email: "jan@turing.com", password: "imabeast", avatar: "https://chess-with-frein-emies-e45d9fb62d80.herokuapp.com/images/dog.jpg")
+user5 = User.create(username: "kat", email: "kat@turing.com", password: "bestinstructorever", avatar: "https://chess-with-frein-emies-e45d9fb62d80.herokuapp.com/images/cat.jpg")
+user6 = User.create(username: "abdul", email: "abdul@turing.com", password: "kingofmod2", avatar: "https://chess-with-frein-emies-e45d9fb62d80.herokuapp.com/images/pixar.jpg")
+user7 = User.create(username: "jeff", email: "jeff@turing.com", password: "iownthisshizz", avatar: "https://chess-with-frein-emies-e45d9fb62d80.herokuapp.com/images/bones.jpg")
 
 friendship1 = Friendship.create(user_id: user1.id, friend_id: user2.id)
 friendship2 = Friendship.create(user_id: user1.id, friend_id: user3.id)
