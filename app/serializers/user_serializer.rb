4906c9c57d4c754fc5d4b48e0f00eb85cf419745
Lance Nelson <lancen04@gmail.com>
2024-10-23 16:12:23 -0400

    fix: corrected users serializer and fixed test

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -4,21 +4,4 @@ class UserSerializer
   set_id :id
   set_type :user
   attributes :username, :email, :avatar
-
-
-  def self.index_json(users)
-    {
-      "data": users.map do |user|
-        {
-          "type": "user",
-          "id": user.id,
-          "attributes": {
-            "username": user.username,
-            "email": user.email,
-            "avatar": user.avatar
-          }
-        }
-      end
-    }
-  end
 end
