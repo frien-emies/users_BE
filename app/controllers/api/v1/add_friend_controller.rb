6e598f9296c516a5c55af6d91ae50e0cd3f1eca1
Lance Nelson <lancen04@gmail.com>
2024-10-24 13:49:53 -0400

    edit: edited add friend logic

--- app/controllers/api/v1/add_friend_controller.rb
+++ app/controllers/api/v1/add_friend_controller.rb
@@ -1,17 +1,36 @@
 class Api::V1::AddFriendController < ApplicationController
+  # def create
+  #   begin 
+  #     user = User.find_by(id: params[:id])
+  #     friend = User.find_by(id: params[:user_id])
+  #     if user && friend
+  #       Friendship.create!(user: user, friend: friend)
+  #       Friendship.create!(user: friend, friend: user)
+  #       render json: { "data": "You've added a friend!" }, status: 201
+  #     else
+  #       render json: { "errors": "One of more of the users do not exist in the database." }, status: 400
+  #     end
+  #   rescue ActiveRecord::RecordInvalid => exception
+  #     render json: ErrorSerializer.new(ErrorMessage.new(exception.message, 400)).serialize_json, status: 400
+  #   end
+  # end
+
   def create
     begin 
-      user = User.find_by(id: params[:id])
-      friend = User.find_by(id: params[:user_id])
-      if user && friend
-        Friendship.create!(user: user, friend: friend)
-        Friendship.create!(user: friend, friend: user)
+      user = User.find(params[:id])
+      friend = User.find(params[:user_id])
+      
+      friendship1 = Friendship.create(user: user, friend: friend)
+      friendship2 = Friendship.create(user: friend, friend: user)
+  
+      if friendship1.valid? && friendship2.valid?
         render json: { "data": "You've added a friend!" }, status: 201
       else
-        render json: { "errors": "One of more of the users do not exist in the database." }, status: 400
+        errors = friendship1.errors.full_messages + friendship2.errors.full_messages
+        render json: ErrorSerializer.new(ErrorMessage.new(errors.join(", "), 400)).serialize_json, status: 400
       end
-    rescue ActiveRecord::RecordInvalid => exception
-      render json: ErrorSerializer.new(ErrorMessage.new(exception.message, 400)).serialize_json, status: 400
+    rescue ActiveRecord::RecordNotFound => exception
+      render json: ErrorSerializer.new(ErrorMessage.new(exception.message, 404)).serialize_json, status: 404
     end
   end
 end
--- spec/requests/api/v1/add_friend_spec.rb
+++ spec/requests/api/v1/add_friend_spec.rb
@@ -41,7 +41,7 @@ RSpec.describe "Add Friend" do
       expect(user1.friendships.count).to be 0
 
       reply = JSON.parse(response.body, symbolize_names: true)[:errors][0]
-      expect(reply[:message]).to eq("Validation failed: Friend can't be the same as user")
+      expect(reply[:message]).to eq("Friend can't be the same as user, Friend can't be the same as user")
       expect(reply[:status]).to eq("400")
     end
 
@@ -63,7 +63,7 @@ RSpec.describe "Add Friend" do
 
       reply = JSON.parse(response.body, symbolize_names: true)[:errors][0]
       
-      expect(reply[:message]).to eq("Validation failed: User has already been taken")
+      expect(reply[:message]).to eq("User has already been taken")
       expect(reply[:status]).to eq("400")
     end
 
@@ -78,14 +78,14 @@ RSpec.describe "Add Friend" do
       headers = {"CONTENT_TYPE" => "application/json"}
       post "/api/v1/users/#{user1.id}/add_friend", params: JSON.generate(post_body), headers: headers
     
-      expect(response.status).to be 400
+      expect(response.status).to be 404
       expect(user1.friendships.count).to be 0
 
       reply = JSON.parse(response.body, symbolize_names: true)
 
       expect(reply).to have_key(:errors)
-      expect(reply[:errors]).to be_a(String)
-      expect(reply[:errors]).to eq("One of more of the users do not exist in the database.")
+      expect(reply[:errors]).to be_an(Array)
+      expect(reply[:errors]).to eq([{:message=>"Couldn't find User with 'id'=12345678", :status=>"404"}])
     end
   end
 end
