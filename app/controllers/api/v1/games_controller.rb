d4ddbf127ecb94fbd1d0d277e2ce4c6d79dc3193
Lance Nelson <lancen04@gmail.com>
2024-10-25 15:05:02 -0400

    edit: rearranged some of games controller

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -2,11 +2,14 @@ class Api::V1::GamesController < ApplicationController
   def create
     user = User.find_by(id: params[:id])
     friend = User.find_by(id: params[:black_player_id])
-
+    
     if user && friend
       new_game = ChessService.get_game_info(user, friend)
       new_game_id = new_game[:game][:id]
-      game = Game.create(id: new_game_id, game: "Chess", avatar: "https://chess-with-frein-emies-e45d9fb62d80.herokuapp.com/images/chess_dab.jpg", status: 0)
+      game = Game.create(id: new_game_id, 
+                          game: "Chess", 
+                          avatar: "https://chess-with-frein-emies-e45d9fb62d80.herokuapp.com/images/chess_dab.jpg", 
+                          status: 0)
 
       if game.users.count < 2
         UserGame.create(user_id: user.id, game_id: new_game_id, player_id: 1)
