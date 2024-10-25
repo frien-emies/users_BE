2cd6d6c142caebd2ad993ee65c8f7223384d0bd8
Lance Nelson <lancen04@gmail.com>
2024-10-25 18:33:44 -0400

    refactored some stuff hehehehe

--- app/controllers/api/v1/friends_controller.rb
+++ app/controllers/api/v1/friends_controller.rb
@@ -8,4 +8,39 @@ class Api::V1::FriendsController < ApplicationController
       render json: FriendsSerializer.friends_json(friends), status: 200
     end
   end
+
+  def create
+    begin 
+      user = User.find(params[:id])
+      friend = User.find(params[:user_id])
+      
+      friendship1 = Friendship.create(user: user, friend: friend)
+      friendship2 = Friendship.create(user: friend, friend: user)
+  
+      if friendship1.valid? && friendship2.valid?
+        render json: { "data": "You've added a friend!" }, status: 201
+      else
+        errors = friendship1.errors.full_messages + friendship2.errors.full_messages
+        render json: ErrorSerializer.new(ErrorMessage.new(errors.join(", "), 400)).serialize_json, status: 400
+      end
+    rescue ActiveRecord::RecordNotFound => exception
+      render json: ErrorSerializer.new(ErrorMessage.new(exception.message, 404)).serialize_json, status: 404
+    end
+  end
+
+  def destroy 
+    user = User.find_by(id: params[:id])
+    friend = User.find_by(id: params[:user_id])
+
+    if friend && user
+      friendship1 = Friendship.find_by(user: user, friend: friend)
+      friendship2 = Friendship.find_by(user: friend, friend: user)
+
+      friendship1.delete
+      friendship2.delete
+      render json: { "data": "You've removed a friend!" }, status: 201
+    else
+      render json: { "errors": "Could not remove friend." }, status: 400
+    end
+  end
 end
--- spec/requests/api/v1/remove_friend_spec.rb
+++ spec/requests/api/v1/remove_friend_spec.rb
@@ -28,4 +28,34 @@ RSpec.describe "Remove Friend" do
       expect(reply).to eq("You've removed a friend!")
     end
   end
+
+  describe "sad path" do 
+    it "returns error json if users do not exist" do 
+      user1 = User.create(username: "bob", email: "bob@turing.com", password: "ihateruby", avatar: "image1_string_goes_here.jpg")
+      user2 = User.create(username: "rob", email: "rob@turing.com", password: "iloveruby", avatar: "image2_string_goes_here.jpg")
+      Friendship.create!(user: user1, friend: user2)
+      Friendship.create!(user: user2, friend: user1)
+      delete_body = ({
+        user_id: 12345
+      })
+
+      expect(user1.friendships.count).to be 1
+  
+      headers = {"CONTENT_TYPE" => "application/json"}
+      delete "/api/v1/users/#{user1.id}/remove_friend", params: JSON.generate(delete_body), headers: headers
+    
+      expect(response).to_not be_successful
+      expect(response.status).to be 400
+      expect(user1.friendships.count).to be 1
+      expect(user2.inverse_friendships.count).to be 1
+      expect(user2.friendships.count).to be 1
+      expect(user1.inverse_friendships.count).to be 1
+
+      reply = JSON.parse(response.body, symbolize_names: true)
+      
+      expect(reply).to have_key(:errors)
+      expect(reply[:errors]).to be_a(String)
+      expect(reply[:errors]).to eq("Could not remove friend.")
+    end
+  end
 end
